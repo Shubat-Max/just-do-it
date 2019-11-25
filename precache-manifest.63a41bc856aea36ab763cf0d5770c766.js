7eb48b7f0a36c3dbf9d51da8904175f345a1a95b
Shubat Max <shubat.max@gmail.com>
2019-11-25 14:41:12 +0300

    Updates

---
+++ precache-manifest.63a41bc856aea36ab763cf0d5770c766.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3359c0a63682a4b46b6bf4a8f88a06",
+    "url": "/just-do-it/index.html"
+  },
+  {
+    "revision": "2b3dd401f630e9486e9c",
+    "url": "/just-do-it/static/css/main.c960ad86.chunk.css"
+  },
+  {
+    "revision": "9cd3ee2e09947bd9a2be",
+    "url": "/just-do-it/static/js/2.c8f6dbd4.chunk.js"
+  },
+  {
+    "revision": "2b3dd401f630e9486e9c",
+    "url": "/just-do-it/static/js/main.3567fa03.chunk.js"
+  },
+  {
+    "revision": "1c1f3329eb1f1e2d2cfb",
+    "url": "/just-do-it/static/js/runtime-main.e83985ed.js"
+  }
+]);
